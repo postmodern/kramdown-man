b9921b0fdc06a6893b563fc93c6d642b32556e42
Postmodern <postmodern.mod3@gmail.com>
2013-05-05 17:59:02 -0700

    Added specs for convert_a.

--- spec/kramdown/converter/man_spec.rb
+++ spec/kramdown/converter/man_spec.rb
@@ -8,6 +8,54 @@ describe Kramdown::Converter::Man do
 
   subject { described_class.send(:new,root,{}) }
 
+  describe "#convert_a" do
+    let(:text) { 'example'             }
+    let(:href) { 'http://example.com/' }
+    let(:doc)  { Kramdown::Document.new("[#{text}](#{href})") }
+    let(:link) { doc.root.children[0].children[0] }
+
+    it "should convert a link into 'text\\n.UR href\\n.UE'" do
+      subject.convert_a(link).should == "#{text}\n.UR #{href}\n.UE"
+    end
+
+    context "when the href begins with mailto:" do
+      let(:text)  { 'Bob'             }
+      let(:email) { 'bob@example.com' }
+      let(:doc)   { Kramdown::Document.new("[#{text}](mailto:#{email})") }
+
+      it "should convert the link into '.MT email\\n.ME'" do
+        subject.convert_a(link).should == "#{text}\n.MT #{email}\n.ME"
+      end
+
+      context "when link is <email>" do
+        let(:email) { 'bob@example.com' }
+        let(:doc)   { Kramdown::Document.new("<#{email}>") }
+
+        it "should convert the link into '.MT email\\n.ME'" do
+          subject.convert_a(link).should == "\n.MT #{email}\n.ME"
+        end
+      end
+    end
+
+    context "when the href begins with man:" do
+      let(:man)  { 'bash' }
+      let(:doc)  { Kramdown::Document.new("[#{man}](man:#{man})") }
+
+      it "should convert the link into '.BR man'" do
+        subject.convert_a(link).should == ".BR #{man}"
+      end
+
+      context "when a section number is specified" do
+        let(:section) { '1' }
+        let(:doc)     { Kramdown::Document.new("[#{man}](man:#{man}(#{section}))") }
+
+        it "should convert the link into '.BR man (section)'" do
+          subject.convert_a(link).should == ".BR #{man} (#{section})"
+        end
+      end
+    end
+  end
+
   describe "#escape" do
     it "should escape '\\' as '\\\\'" do
       subject.escape("hello\\ world").should == "hello\\\\ world"
