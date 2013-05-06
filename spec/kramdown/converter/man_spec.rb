5725ff4696ba40d88d2ca12f31a3774832535032
Postmodern <postmodern.mod3@gmail.com>
2013-05-05 20:58:30 -0700

    Added specs for convert_ol_li.

--- spec/kramdown/converter/man_spec.rb
+++ spec/kramdown/converter/man_spec.rb
@@ -249,6 +249,30 @@ Hello world.
   end
 
   describe "#convert_ol_li" do
+    let(:content) { 'hello world'                           }
+    let(:doc)     { Kramdown::Document.new("1. #{content}") }
+    let(:li)      { doc.root.children[0].children[0]        }
+
+    it "should convert the first p element to '.IP \\\\n+[step0]\\n...'" do
+      subject.convert_ol_li(li).should == ".IP \\n+[step0]\n#{content}"
+    end
+
+    context "with multiple multiple paragraphs" do
+      let(:text1) { 'hello' }
+      let(:text2) { 'world' }
+      let(:doc)   { Kramdown::Document.new("1. #{text1}\n\n   #{text2}") }
+
+      it "should convert the other p elements to '.IP \\\\n\\n...'" do
+        p li
+
+        subject.convert_ol_li(li).should == [
+          ".IP \\n+[step0]",
+          text1,
+          ".IP \\n",
+          text2
+        ].join("\n")
+      end
+    end
   end
 
   describe "#convert_abbreviation" do
