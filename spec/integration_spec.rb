77d555a923629009b849a6dae00dcd7ecc3eac56
Postmodern <postmodern.mod3@gmail.com>
2020-07-14 21:38:09 -0700

    Added an integration spec to ensure Kramdown::Document#to_man works as expected.

---
+++ spec/integration_spec.rb
@@ -0,0 +1,19 @@
+require_relative 'spec_helper'
+
+require 'kramdown/man'
+
+describe Kramdown::Document do
+  let(:man_dir)       { File.expand_path('../../man',__FILE__) }
+  let(:markdown_path) { File.join(man_dir,'kramdown-man.1.md') }
+  let(:markdown)      { File.read(markdown_path)               }
+
+  subject { described_class.new(markdown) }
+
+  describe "#to_man" do
+    it "must return the same output as Kramdown::Converter::Man" do
+      output, warnings = Kramdown::Converter::Man.convert(subject.root)
+
+      expect(subject.to_man).to be == output
+    end
+  end
+end
